dcfcd9d9305669cba2bb58a6bb9a7d22b4b6f84c
Putra Gilang <putragilanq@protonmail.com>
2022-04-13 22:21:16 +0700

    updet nama warna

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 hijau='\33[32;1m' # HIJAU
-merah='\33[0;36m' # BIRU MUDA
+birmud='\33[0;36m' # BIRU MUDA
 kuning='\33[1;33m' # KUNING
-ungu='\033[95m' # UNNGU
+ungu='\033[95m' # UNGU BAND
 biru='\033[94m' # BIRU
 reset='\033[0m' # CLEAR COLOR
 
