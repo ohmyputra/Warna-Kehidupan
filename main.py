ee7ef927a94d20eb5393a324ec5f2e2d1606f1fd
Putra Gilang <putragilanq@protonmail.com>
2022-04-13 22:19:51 +0700

    warna-warni

---
+++ main.py
@@ -0,0 +1,16 @@
+hijau='\33[32;1m' # HIJAU
+merah='\33[0;36m' # BIRU MUDA
+kuning='\33[1;33m' # KUNING
+ungu='\033[95m' # UNNGU
+biru='\033[94m' # BIRU
+reset='\033[0m' # CLEAR COLOR
+
+def main():
+	print(f"{hijau}Aku Hijau")
+	print(f"{merah}Aku Biru Muda")
+	print(f"{kuning}Aku Kuning")
+	print(f"{ungu}Aku Ungu")
+	print(f"{biru}Aku Biru")
+	print(f"{reset}Aku Penghapus Semua Warna.")
+	
+main()
